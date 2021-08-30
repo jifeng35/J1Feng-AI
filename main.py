980e11c7630b0af2da242dac42f00752ee786f38
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-08-30 15:25:09 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn import feature_extraction
+from sklearn.feature_extraction import DictVectorizer
+
+
 def load():
     iris = load_iris()
     #  print("鸢尾花数据集的描述为:\n", iris["DESCR"])
@@ -14,5 +17,16 @@ def load():
 # 　返回值为字典数据类型,共有五个键 data:特征数据数组 target:标签数组 DESCR:数据描述
 #   feature_names:特征名 target_names:标签名
 
+def dict_demo():
+    data_dict = [{"City": "Beijing", "Temperature": 20},
+                 {"City": "Shanghai", "Temperature": 30},
+                 {"City": "Shenzhen", "Temperature": 25}]
+    transfer = DictVectorizer(sparse=False)  # 默认返回稀疏矩阵,省略0的存储空间
+    new_data = transfer.fit_transform(data_dict)
+    print(data_dict)
+    print(new_data)
+
+
 if __name__ == '__main__':
     load()
+    dict_demo()
